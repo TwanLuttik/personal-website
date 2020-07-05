cff35c1bbd364ccc7e2762e3b075acf01ce1c682
Twan Luttik <twanluttik@gmail.com>
2020-07-05 11:29:32 -0700

    remove unused code

--- generate.js
+++ generate.js
@@ -48,62 +48,6 @@ let components = "";
 })()
 
 
-// async function run() {
-//   // Clear the variables before starting
-//   imports = '';
-//   components = '';
-
-//   // Console log
-//   console.log('Converting svg into vue svg components\n\n')
-
-//   // Get all the svg's
-//   let icons = await fs.readdirSync(SVG_FOLDER);
-
-//   // Filter only for .svg extenstions
-//   icons = icons.filter((item) => item.includes(".svg"));
-//   // Loop trough the svg files
-
-//   for await (let icon_item of icons) {
-
-//     // Read the icon
-//     let icon = await fs.readFileSync(`${SVG_FOLDER}/${icon_item}`, { encoding: "utf8" });
-
-//     icon = await validateSVG(icon);
-//     // icon.replace('class=""', "");
-
-//     // Change extension from .svg to .vue
-//     icon_item = icon_item.replace(".svg", ".vue");
-
-//     // Wrap it inside a template
-//     let parsed_icon = `<template>${icon}</template>`;
-
-//     // Create vue file
-//     await fs.writeFileSync(`${OUTPUT_FOLDER}/${icon_item}`, parsed_icon, { encoding: "utf8" });
-
-//     // Console log
-//     if (icons.length <= 10) console.log(`${icon_item} created`);
-
-//     // Components string builder
-//     components = components + icon_item.split(".vue")[0] + ",";
-
-//     // imports string builder
-//     imports = imports + `import ${icon_item.replace(".vue", "")} from './GeneratedIcons/${icon_item}';`;
-//   }
-
-//   //  Console log
-//   if (icons.length >= 10) console.log('...')
-
-//   // `build the new icon template
-//   let new_icon_template = ICON_TEMPLATE.replace("${imports}", imports).replace("${components}", components);
-
-//   // Build Icon file
-//   await fs.writeFileSync(`${ROOT_FOLDER}/Icon.vue`, new_icon_template, { encoding: "utf8" });
-//   console.log(`\n🌟 Generated ${icons.length + 1} files;\n\n`);
-//   console.log(`Ready - ${new Date().toLocaleString()}`)
-// };
-
-
-
 /**
  * @description 
  */
