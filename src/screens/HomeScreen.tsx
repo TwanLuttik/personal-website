d66b038b7f46dbf1ab7571b6088f91d4fd90e5b7
Twan L <twanluttik@gmail.com>
2020-12-26 19:49:09 -0800

    phone view friendly, added some about me text and changed layout a little bit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,7 +6,7 @@ import { Center, Icon, Spacer } from '../ui';
 interface HomeScreenProps {}
 
 export const HomeScreen: React.FunctionComponent<HomeScreenProps> = (props) => {
-  const ICON_SIZE = 35;
+  const ICON_SIZE = 25;
 
   const go = (a) => {
     window.open(a);
@@ -16,6 +16,11 @@ export const HomeScreen: React.FunctionComponent<HomeScreenProps> = (props) => {
     <HomeScreenBody>
       <Spacer amount={10} />
       <Title>Twan Luttik</Title>
+      <Spacer amount={5} />
+      <AboutText>
+        Sooooo.... This guy is 21 years old and is passioned about IT and more specific, programming. He likes being
+        outside te house than just work.
+      </AboutText>
       <Spacer amount={30} />
 
       <Bio>
@@ -36,7 +41,9 @@ export const HomeScreen: React.FunctionComponent<HomeScreenProps> = (props) => {
         </Box>
       </Bio>
 
-      <Spacer amount={80} />
+      {/* <Spacer amount={80} /> */}
+      <div style={{ display: 'flex', flex: 1 }}></div>
+
       <IconsContainer>
         <IconButton onClick={() => go('https://github.com/twanluttik')} name="github" size={ICON_SIZE} color="white" />
         <IconButton
@@ -59,6 +66,8 @@ export const HomeScreen: React.FunctionComponent<HomeScreenProps> = (props) => {
           color="white"
         />
       </IconsContainer>
+      <Spacer amount={15} />
+      <Madeby>Its not made with love, Its made with code.</Madeby>
     </HomeScreenBody>
   );
 };
@@ -66,6 +75,11 @@ export const HomeScreen: React.FunctionComponent<HomeScreenProps> = (props) => {
 const HomeScreenBody = styled.div`
   background-color: black;
   color: white;
+
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 650px;
 `;
 
 const IconButton = styled(Icon)<any>`
@@ -89,7 +103,7 @@ const IconsContainer = styled.div`
 const SoftTitle = styled.p`
   font-weight: 700;
   background-color: #f3ec78;
-  background-image: linear-gradient(45deg, #397dd5, #625ecc);
+  background-image: linear-gradient(15deg, #6195da, #625ecc);
   background-size: 100%;
   -webkit-background-clip: text;
   -moz-background-clip: text;
@@ -122,17 +136,36 @@ const Bio = styled(Center)`
 const Box = styled.p`
   border-radius: 12px;
   cursor: pointer;
-  padding: 15px 20px;
+  padding: 10px 18px;
   width: fit-content;
   background-color: black;
-  color: white;
-  border: solid 2px white;
+  color: #ffffff50;
+  font-size: 14px;
+  border: solid 2px #ffffff50;
 
   transition: transform 0.2s ease, background-color 0.2s ease, color 0.2s ease, border 0.2s ease;
   &:hover {
     background-color: white;
-    transform: scale(1.1);
     color: black;
     border: solid 2px black;
   }
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const AboutText = styled.p`
+  max-width: 450px;
+  text-align: center;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+const Madeby = styled.p`
+  text-align: center;
+  font-weight: 600;
+  padding-bottom: 10px;
+  opacity: 0.3;
+  font-size: 14px;
+  font-style: italic;
 `;
