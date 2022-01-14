67f4e977b83d3e1ed650230ae61614912f89ab2a
twanluttik <twanluttik@gmail.com>
2022-01-14 13:51:50 -0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Footer } from '../components/Footer';
 import styled from 'styled-components';
-import { Head } from 'next/document';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <PageBody>
@@ -18,7 +18,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 const PageBody = styled.div`
   background-color: #090e1c;
   height: calc(100% - 60px);
-  //height: 100%;
 `;
 
 export default MyApp;
