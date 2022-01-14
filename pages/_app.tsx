dd093d488bb8495d8b4d1c2195a2152f400fabee
twanluttik <twanluttik@gmail.com>
2022-01-14 13:49:54 -0800

    updated the theme color for safari

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Footer } from '../components/Footer';
 import styled from 'styled-components';
+import { Head } from 'next/document';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <PageBody>
+		<Head>
+			<title>Twan Luttik</title>
+			<meta name="theme-color" content="#090e1c" />
+		</Head>
 		<Component {...pageProps} />
 		<Footer />
 	</PageBody>;
@@ -12,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 const PageBody = styled.div`
   background-color: #090e1c;
-	height: calc(100% - 60px);
+  height: calc(100% - 60px);
   //height: 100%;
 `;
 
