dda1099f556967e1a7c53ea918d91fc1fb9866e9
TwanLuttik <twanluttik@gmail.com>
2023-11-18 03:46:21 -0800

    updated website

---
+++ src/app/easy_qr_scan-policy/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+	return (
+		<div className="bg-black h-full w-full">
+			<p>Easy QR Scan - Policy</p>
+			<p>There is no policy for this app.</p>
+		</div>
+	);
+};
+
+export default Page;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<div className="bg-black h-full w-full justify-center items-center flex">
+			<div className="flex flex-col">
+				<p className="font-semibold text-lg">Twan Luttik</p>
+				<p className="font-light mt-1">
+					... A simple software engineer that likes katsu curry
+				</p>
+			</div>
+		</div>
+	);
+}
