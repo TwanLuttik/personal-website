8e0f89ff3d2b2cd23c7b336fdebe45e82943f585
TwanLuttik <twanluttik@gmail.com>
2024-03-13 22:36:35 -0700

    added plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Twan Luttik',
@@ -14,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script defer data-domain="twanluttik.com" src="https://plausible.io/js/script.js"></Script>
       <body className={GeistSans.className}>{children}</body>
     </html>
   )
